20e4791f7a61248eb382724d03df9abcb09f1edb
Jennifer Beckingham <jenniferbeckingham@Jennifers-MacBook-Air.local>
2020-06-16 17:02:25 +0100

    Try using live domain to see if build now successful

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
-
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "86fdf77cbf704aacb43c9e7a6b30e105";
-export const redirectUri = process.env.REACT_APP_SITE_DOMAIN + "/redirect";
+export const redirectUri = "the-music-round.netlify.app/redirect";
 export const scopes = ["streaming", "user-read-email", "user-read-private"];
